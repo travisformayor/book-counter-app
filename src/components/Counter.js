964accfd7c002c80dabcbb5049a239c88da90279
Travis <travisformayor@users.noreply.github.com>
2020-09-06 18:38:24 -0700

    read current counter value

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,5 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-export default function Counter() {
-  return <div>Counter goes here</div>;
+export default function Counter({ contract }) {
+  const [initialValue, setInitialValue] = useState(null);
+
+  useEffect(() => {
+    if (!initialValue) {
+      (async () => {
+        const initialValue = await contract.methods.value().call();
+        setInitialValue(initialValue);
+      })();
+    }
+  });
+
+  useEffect(() => {
+    console.log(initialValue);
+  }, [initialValue]);
+
+  return (
+    <div>{initialValue ? `Counter value: ${initialValue}` : 'Loading...'}</div>
+  );
 }
